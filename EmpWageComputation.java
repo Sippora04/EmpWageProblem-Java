2c4f2eee38816e00d03c85656e067497a6a1f119
Sippora04 <sippora.toppo95@gmail.com>
2020-06-10 10:57:45 +0530

    Added: Employee Attendance

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,8 +1,13 @@
 package com.Employee;
+import java.util.Random;
 public class EmpWageComputation
 {
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to Employee Wage Computation/Problem");
+		Random randNum = new Random();
+      int empCheck = randNum.nextInt()%2;
+		if(empCheck==0) { System.out.println(empCheck+"Absent"); }
+		else System.out.println("Present");
 	}
 }
